9010d609e2885655cb85e7e1f38e7caf44e1f597
Matti Jauhiainen <matti.tapio.jauhiainen@gmail.com>
2026-01-15 18:49:59 +0200

    Filter out small quakes

--- public/data.js
+++ public/data.js
@@ -1,4 +1,10 @@
 export async function fetchEarthquakeData() {
   const response = await fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson');
-  return await response.json();
+  const data = await response.json();
+
+  // Filter out small earthquakes. Data is from USGS and will be very biased for North
+  // America if we consider all magnitudes.
+  data.features = data.features.filter(earthquake => earthquake.properties.mag >= 4.0);
+
+  return data;
 }
